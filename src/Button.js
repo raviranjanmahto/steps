9ad02f46c2a991c92891fda5b0bb1a98b56b59c9
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-07-20 20:10:25 +0530

    separated components and refactor code

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+export default function Button({ bgColor, textColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
